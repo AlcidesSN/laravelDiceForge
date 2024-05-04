a1216471fe7541b4977f3adf1eed03e9ff84eb24
AlcidesSN <alcidessandoli@gmail.com>
2024-05-04 13:49:12 -0300

    pagina projeto - esquema

--- resources/views/projeto.blade.php
+++ resources/views/projeto.blade.php
@@ -5,9 +5,27 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href='https://fonts.googleapis.com/css?family=JetBrains Mono' rel='stylesheet'>
+    <link rel="stylesheet" href="/css/projeto.css">
     <title>Projeto</title>
 </head>
 <body>
+    <body>
+        <header>
+            <a href="/" class="logo"><img  src="/img/logo-unifil.png" alt="logo-unifil"></a>
+            <nav>
+                <ul>
+                    <li><a href="#">Início</a></li>
+                    <li><a href="/projeto.html">Projeto</a></li>
+                    <li><a href="/sobre.html">Sobre</a></li>
+                </ul>
+            </nav>
+        </header>
+        <div>
+            <h1>Em breve</h1>
+        </div>
+        <footer>
+            <p>&copy; 2024 Dice Forge</p>
+        </footer>
 
-</body>
+    </body>
 </html>
